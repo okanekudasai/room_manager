fdbe154d168a07567603f650be6baef6b8f4a48e
opi6opi6 <opi6@hanmail.net>
2023-01-20 01:50:10 +0900

    ss

--- src/main/java/manager/SocketHandler.java
+++ src/main/java/manager/SocketHandler.java
@@ -1,9 +1,15 @@
 package manager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -11,11 +17,17 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.google.gson.Gson;
 
-@Component
+@RestController
+@CrossOrigin("*")
 public class SocketHandler extends TextWebSocketHandler {
-
-	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+	Gson gson = new Gson();
+	Map<String, CopyOnWriteArrayList<WebSocketSession>> sessionMap = new HashMap<String, CopyOnWriteArrayList<WebSocketSession>>();
 	List<RoomDto> roomList = new CopyOnWriteArrayList<>();
+	
+	@PostMapping("/here")
+	void here(@RequestBody String here) {
+		System.out.println(here);
+	}
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -27,23 +39,23 @@ public class SocketHandler extends TextWebSocketHandler {
 		}
 		System.out.println();
 		System.out.println("이번세션 : " + sessions.indexOf(session));
+		System.out.println();
 	}
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		System.out.println(message.getPayload());
-		Gson gson = new Gson();
-//		HashMap<String, String> jsonObject = (JSONObject) parser.parse(message.getPayload());
-//		System.out.println(jsonObject);
-//		switch(data.get("event")) {
-//		case "makeRoom":
-//			for (WebSocketSession webSocketSession : sessions) {
-//				if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
-//					webSocketSession.sendMessage(message);
-//				}
-//			}
-//			break;
-//		}
+		Map<String, String> data = gson.fromJson(message.getPayload(), Map.class);
+		switch(data.get("event")) {
+		case "makeRoom":
+			for (WebSocketSession webSocketSession : sessions) {
+				if (webSocketSession.isOpen()/* && !session.getId().equals(webSocketSession.getId()) */) {
+					roomList.add(new RoomDto(data.get("roomName"), data.get("nickName"), new ArrayList<String>()));
+					webSocketSession.sendMessage(new TextMessage(gson.toJson(roomList)));
+				}
+			}
+			break;
+		}
+		System.out.println();
 	}
 	
 	@Override
@@ -55,6 +67,7 @@ public class SocketHandler extends TextWebSocketHandler {
 		for (WebSocketSession s : sessions) {
 			System.out.print(sessions.indexOf(s) + " ");
 		}
+		System.out.println();
 		System.out.println();
 	}	
 	
